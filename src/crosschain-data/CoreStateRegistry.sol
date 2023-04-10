408b2442e9eab3be35b299bbfcb0ba20ab5c5a7b
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2023-04-10 17:53:58 +0100

    fix: typo

--- src/crosschain-data/CoreStateRegistry.sol
+++ src/crosschain-data/CoreStateRegistry.sol
@@ -10,7 +10,7 @@ import "../utils/DataPacking.sol";
 
 /// @title Cross-Chain AMB Aggregator
 /// @author Zeropoint Labs
-/// @notice stores, senxwxwds & process message sent via various messaging ambs.
+/// @notice stores, sends & process message sent via various messaging ambs.
 contract CoreStateRegistry is BaseStateRegistry, ICoreStateRegistry {
     /*///////////////////////////////////////////////////////////////
                             STATE VARIABLES
