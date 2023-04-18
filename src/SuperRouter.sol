6a53b14dbd9478495a3e29caf6085ac7607758df
blackbigswan <blackbigswan@gmail.com>
2023-04-19 01:58:06 +0200

    chore: remove ERC1155Receiver interface from SuperRouter

--- src/SuperRouter.sol
+++ src/SuperRouter.sol
@@ -1198,27 +1198,3 @@ contract SuperRouter is ISuperRouter, LiquidityHandler {
     }
 
 }
-
-/// @notice A generic interface for a contract which properly accepts ERC1155 tokens.
-/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC1155.sol)
-abstract contract ERC1155TokenReceiver {
-    function onERC1155Received(
-        address,
-        address,
-        uint256,
-        uint256,
-        bytes calldata
-    ) external virtual returns (bytes4) {
-        return this.onERC1155Received.selector;
-    }
-
-    function onERC1155BatchReceived(
-        address,
-        address,
-        uint256[] calldata,
-        uint256[] calldata,
-        bytes calldata
-    ) external virtual returns (bytes4) {
-        return this.onERC1155BatchReceived.selector;
-    }
-}
