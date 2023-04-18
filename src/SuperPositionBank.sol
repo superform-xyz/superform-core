c0cd9dc9808d4dc09de8fc2cfac8bb407dd1e9c3
blackbigswan <blackbigswan@gmail.com>
2023-04-19 01:51:47 +0200

    feat: add 1155 interfaces to SPBank and SuperRouter, fix: safeTransferFrom bug

--- src/SuperPositionBank.sol
+++ src/SuperPositionBank.sol
@@ -176,3 +176,28 @@ contract SuperPositionBank is ERC165 {
         return super.supportsInterface(interfaceId);
     }
 }
+
+
+/// @notice A generic interface for a contract which properly accepts ERC1155 tokens.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC1155.sol)
+abstract contract ERC1155TokenReceiver {
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+}
--- src/SuperRouter.sol
+++ src/SuperRouter.sol
@@ -1196,4 +1196,29 @@ contract SuperRouter is ISuperRouter, LiquidityHandler {
 
         return true;
     }
+
+}
+
+/// @notice A generic interface for a contract which properly accepts ERC1155 tokens.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC1155.sol)
+abstract contract ERC1155TokenReceiver {
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
 }
