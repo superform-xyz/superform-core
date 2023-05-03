508713a56f83c9e46037885e83f520adb3f6cdac
blackbigswan <blackbigswan@gmail.com>
2023-05-03 14:12:20 +0200

    feat: PoC scaffold finished, FormStateRegistry comms with Timelock

--- src/forms/form_keeper/FormStateRegistry.sol
+++ src/forms/form_keeper/FormStateRegistry.sol
@@ -5,6 +5,7 @@ import {ISuperRBAC} from "../../interfaces/ISuperRBAC.sol";
 import {ISuperRegistry} from "../../interfaces/ISuperRegistry.sol";
 import {IERC4626Timelock} from ".././interfaces/IERC4626Timelock.sol";
 import {Error} from "../../utils/Error.sol";
+import "../../utils/DataPacking.sol";
 
 contract FormStateRegistry {
 
@@ -13,7 +14,6 @@ contract FormStateRegistry {
     bytes32 public constant TOKEN_BANK_ROLE = keccak256("FORM_KEEPER_ROLE");
 
     ISuperRegistry public superRegistry;
-    IERC4626Timelock public form;
 
     modifier onlyFormKeeper() {
         if (
@@ -22,9 +22,8 @@ contract FormStateRegistry {
         _;
     }
 
-    constructor(address superRegistry_, address form_) {
+    constructor(address superRegistry_) {
         superRegistry = ISuperRegistry(superRegistry_);
-        // form = IERC4626Timelock(form_); /// select that from SuperFormFactory
     }
 
     function receivePayload(uint256 payloadId, uint256 superFormId) external onlyFormKeeper {
@@ -32,7 +31,8 @@ contract FormStateRegistry {
     }
 
     function initPayload(uint256 payloadId) external onlyFormKeeper {
-        form.processUnlock(payloadId);
+        (address form_, , ) = _getSuperForm(payloadStore[payloadId]);
+        IERC4626Timelock(form_).processUnlock(payloadId);
         delete payloadStore[payloadId];
     }
 
