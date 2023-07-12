4dfcce033d869181a93251a0541fa3e9b4aa1a0c
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2023-07-12 11:32:56 +0100

    add withdraw case

---
+++ src/test/scenarios-withdraw-singleXChainSingleVaultWithdraw/4626.Timelocked.Native.Slippage.L2.AMB12.t.sol
@@ -0,0 +1,82 @@
+/// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.19;
+
+// Contracts
+import "../../types/LiquidityTypes.sol";
+import "../../types/DataTypes.sol";
+
+// Test Utils
+import "../utils/ProtocolActions.sol";
+import "../utils/AmbParams.sol";
+
+contract SXSVWTimelockedNativeSlippageL2AMB12 is ProtocolActions {
+    function setUp() public override {
+        super.setUp();
+        /*//////////////////////////////////////////////////////////////
+                !! WARNING !!  DEFINE TEST SETTINGS HERE
+    //////////////////////////////////////////////////////////////*/
+        AMBs = [1, 2];
+
+        CHAIN_0 = OP;
+        DST_CHAINS = [ARBI];
+
+        /// @dev define vaults amounts and slippage for every destination chain and for every action
+        TARGET_UNDERLYINGS[ARBI][0] = [0];
+
+        TARGET_VAULTS[ARBI][0] = [1]; /// @dev id 0 is normal 4626
+
+        TARGET_FORM_KINDS[ARBI][0] = [1];
+
+        /// @dev define vaults amounts and slippage for every destination chain and for every action
+        TARGET_UNDERLYINGS[ARBI][1] = [0];
+
+        TARGET_VAULTS[ARBI][1] = [1]; /// @dev id 0 is normal 4626
+
+        TARGET_FORM_KINDS[ARBI][1] = [1];
+
+        AMOUNTS[ARBI][0] = [421412];
+        AMOUNTS[ARBI][1] = [421412];
+
+        MAX_SLIPPAGE[ARBI][0] = [1000];
+        MAX_SLIPPAGE[ARBI][1] = [1000];
+
+        /// @dev 1 for socket, 2 for lifi
+        LIQ_BRIDGES[ARBI][0] = [2];
+        LIQ_BRIDGES[ARBI][1] = [2];
+
+        vm.selectFork(FORKS[CHAIN_0]);
+
+        actions.push(
+            TestAction({
+                action: Actions.Deposit,
+                multiVaults: false, //!!WARNING turn on or off multi vaults
+                user: 0,
+                testType: TestType.Pass,
+                revertError: "",
+                revertRole: "",
+                slippage: 224, // 0% <- if we are testing a pass this must be below each maxSlippage,
+                multiTx: false,
+                ambParams: generateCoreStateRegistryParams(DST_CHAINS, AMBs),
+                msgValue: estimateMsgValue(DST_CHAINS, AMBs, generateExtraData(AMBs)),
+                externalToken: 3 // 0 = DAI, 1 = USDT, 2 = WETH
+            })
+        );
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        SCENARIO TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function test_scenario() public {
+        for (uint256 act = 0; act < actions.length; act++) {
+            TestAction memory action = actions[act];
+            MultiVaultsSFData[] memory multiSuperFormsData;
+            SingleVaultSFData[] memory singleSuperFormsData;
+            MessagingAssertVars[] memory aV;
+            StagesLocalVars memory vars;
+            bool success;
+
+            _runMainStages(action, act, multiSuperFormsData, singleSuperFormsData, aV, vars, success);
+        }
+    }
+}
