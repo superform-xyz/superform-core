c5c89c638d94f1906dbe88efa96dcef02ae6ec47
smitrajput <smitrajputtd@gmail.com>
2023-07-14 18:48:51 +0530

    :feat: gas-optimise DataLib with yul

--- src/libraries/DataLib.sol
+++ src/libraries/DataLib.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {Error} from "../utils/Error.sol";
 
+/// @dev rationale for "memory-safe" assembly: https://docs.soliditylang.org/en/v0.8.20/assembly.html#memory-safety
 library DataLib {
     function packTxInfo(
         uint8 txType_,
@@ -12,12 +13,14 @@ library DataLib {
         address srcSender_,
         uint64 srcChainId_
     ) internal pure returns (uint256 txInfo) {
-        txInfo = uint256(txType_);
-        txInfo |= uint256(callbackType_) << 8;
-        txInfo |= uint256(multi_) << 16;
-        txInfo |= uint256(registryId_) << 24;
-        txInfo |= uint256(uint160(srcSender_)) << 32;
-        txInfo |= uint256(srcChainId_) << 192;
+        assembly ("memory-safe") {
+            txInfo := txType_
+            txInfo := or(txInfo, shl(8, callbackType_))
+            txInfo := or(txInfo, shl(16, multi_))
+            txInfo := or(txInfo, shl(24, registryId_))
+            txInfo := or(txInfo, shl(32, srcSender_))
+            txInfo := or(txInfo, shl(192, srcChainId_))
+        }
     }
 
     function decodeTxInfo(
@@ -55,15 +58,36 @@ library DataLib {
     /// @return chainIds_ are the chain ids
     function getSuperForms(
         uint256[] memory superFormIds_
-    ) internal pure returns (address[] memory, uint32[] memory, uint64[] memory) {
-        address[] memory superForms_ = new address[](superFormIds_.length);
-        uint32[] memory formIds_ = new uint32[](superFormIds_.length);
-        uint64[] memory chainIds_ = new uint64[](superFormIds_.length);
-        for (uint256 i = 0; i < superFormIds_.length; i++) {
-            (superForms_[i], formIds_[i], chainIds_[i]) = getSuperForm(superFormIds_[i]);
-        }
+    ) internal pure returns (address[] memory superForms_, uint32[] memory formIds_, uint64[] memory chainIds_) {
+        superForms_ = new address[](superFormIds_.length);
+        formIds_ = new uint32[](superFormIds_.length);
+        chainIds_ = new uint64[](superFormIds_.length);
 
-        return (superForms_, formIds_, chainIds_);
+        assembly ("memory-safe") {
+            // pointer to the end of the superFormIds_ array (shl(5, mload(superFormIds_)) == mul(32, mload(superFormIds_))
+            let end := add(add(superFormIds_, 0x20), shl(5, mload(superFormIds_)))
+            // initialize pointers for all the 4 arrays
+            let i := add(superFormIds_, 0x20)
+            let j := add(superForms_, 0x20)
+            let k := add(formIds_, 0x20)
+            let l := add(chainIds_, 0x20)
+
+            let superFormId := 0
+            for {} 1 {} {
+                superFormId := mload(i)
+                // execute what getSuperForm() does on a single superFormId and store the results in the respective arrays
+                mstore(j, superFormId)
+                mstore(k, shr(160, superFormId))
+                mstore(l, shr(192, superFormId))
+                // increment pointers
+                i := add(i, 0x20)
+                j := add(j, 0x20)
+                k := add(k, 0x20)
+                l := add(l, 0x20)
+                // check if we've reached the end of the array
+                if iszero(lt(i, end)) { break }
+            }
+        }
     }
 
     /// @dev validates if the superFormId_ belongs to the chainId_
@@ -94,8 +118,10 @@ library DataLib {
         uint32 formBeaconId_,
         uint64 chainId_
     ) internal pure returns (uint256 superFormId_) {
-        superFormId_ = uint256(uint160(superForm_));
-        superFormId_ |= uint256(formBeaconId_) << 160;
-        superFormId_ |= uint256(chainId_) << 192;
+        assembly ("memory-safe") {
+            superFormId_ := superForm_
+            superFormId_ := or(superFormId_, shl(160, formBeaconId_))
+            superFormId_ := or(superFormId_, shl(192, chainId_))
+        }
     }
 }
