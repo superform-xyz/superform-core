1a69ea5bcaa6ccdf7b2cea13d352f6c045e52e96
Subhasish Goswami <43542047+subhasishgoswami@users.noreply.github.com>
2023-07-17 15:32:35 +0000

    test(superform-forms): test ERC4626Form

---
+++ src/test/superform-forms/superform-factory.ERC4626Form.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+import {ISuperFormFactory} from "../../interfaces/ISuperFormFactory.sol";
+import {ISuperRegistry} from "../../interfaces/ISuperRegistry.sol";
+import {SuperFormFactory} from "../../SuperFormFactory.sol";
+import {FactoryStateRegistry} from "../../crosschain-data/extensions/FactoryStateRegistry.sol";
+import {ERC4626Form} from "../../forms/ERC4626Form.sol";
+import {VaultMock} from "../mocks/VaultMock.sol";
+import {MockERC20} from "../mocks/MockERC20.sol";
+import "../utils/BaseSetup.sol";
+import "../utils/Utilities.sol";
+import {Error} from "../../utils/Error.sol";
+
+contract SuperFormERC4626FormTest is BaseSetup {
+
+    uint64 internal chainId = ETH;
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+    /// @dev Test Yield Token
+    function test_superformYieldTokenName() public {
+        vm.startPrank(deployer);
+        
+        vm.selectFork(FORKS[chainId]);
+
+        /// @dev Setting Vaults And Yield Tokens
+        address superRegistry = getContract(chainId, "SuperRegistry");
+        MockERC20 asset = new MockERC20("Mock ERC20 Token", "Mock", address(this), uint256(1000));
+        VaultMock vault = new VaultMock(asset, "Mock Vault", "Mock");
+
+        /// @dev Deploying Forms
+        ERC4626Form formImplementation = new ERC4626Form(superRegistry);
+        formImplementation.initialize(superRegistry, address(vault));
+
+        string memory vaultName = formImplementation.superformYieldTokenName();
+
+        assertEq(
+            vaultName,
+            "Superform Mock"
+        );
+    }
+}
