abaa84f3ee6c153caae30f57707ca5cb71b954d3
smitrajput <smitrajputtd@gmail.com>
2023-07-21 12:25:45 +0530

    test: reverts for broadcastPayload, setters

--- src/test/crosschain-data-adapters/CelerImplementation.t.sol
+++ src/test/crosschain-data-adapters/CelerImplementation.t.sol
@@ -8,6 +8,7 @@ import {ISuperRegistry} from "../../interfaces/ISuperRegistry.sol";
 import {IAmbImplementation} from "../../interfaces/IAmbImplementation.sol";
 import {CelerImplementation} from "../../crosschain-data/adapters/celer/CelerImplementation.sol";
 import {CoreStateRegistry} from "../../crosschain-data/extensions/CoreStateRegistry.sol";
+import {Error} from "../../utils/Error.sol";
 
 contract CelerImplementationTest is BaseSetup {
     /// @dev event emitted from CelerMessageBus on ETH (CELER_BUS)
@@ -17,6 +18,7 @@ contract CelerImplementationTest is BaseSetup {
     uint64 internal chainId = ETH;
     ISuperRegistry public superRegistry;
     CelerImplementation celerImplementation;
+    address public bond;
 
     function setUp() public override {
         super.setUp();
@@ -24,12 +26,26 @@ contract CelerImplementationTest is BaseSetup {
         vm.selectFork(FORKS[ETH]);
         superRegistry = ISuperRegistry(getContract(chainId, "SuperRegistry"));
         celerImplementation = CelerImplementation(payable(superRegistry.getAmbAddress(3)));
+        /// @dev malicious caller
+        bond = address(7);
+        /// @dev (who's a brokie)
+        vm.deal(bond, 1 ether);
+
         vm.startPrank(deployer);
     }
 
     function test_setCelerBus() public {
         celerImplementation.setCelerBus(CELER_BUS);
         assertEq(address(celerImplementation.messageBus()), CELER_BUS);
+    }
+
+    function test_revert_setCelerBus_invalidMessageBus_invalidCaller() public {
+        vm.expectRevert();
+        celerImplementation.setCelerBus(address(0));
+
+        vm.expectRevert(Error.NOT_PROTOCOL_ADMIN.selector);
+        vm.prank(bond);
+        celerImplementation.setCelerBus(CELER_BUS);
     }
 
     function test_setReceiver() public {
@@ -40,6 +56,18 @@ contract CelerImplementationTest is BaseSetup {
         assertEq(celerImplementation.authorizedImpl(137), getContract(137, "CelerImplementation"));
     }
 
+    function test_revert_setReceiver_invalidChainId_invalidAuthorizedImpl_invalidCaller() public {
+        vm.expectRevert(Error.INVALID_CHAIN_ID.selector);
+        celerImplementation.setReceiver(0, getContract(10, "CelerImplementation"));
+
+        vm.expectRevert(Error.ZERO_ADDRESS.selector);
+        celerImplementation.setReceiver(10, address(0));
+
+        vm.expectRevert(Error.NOT_PROTOCOL_ADMIN.selector);
+        vm.prank(bond);
+        celerImplementation.setReceiver(10, getContract(10, "CelerImplementation"));        
+    }
+
     function test_setChainId() public {
         celerImplementation.setChainId(10, 10); /// optimism
         celerImplementation.setChainId(137, 137); /// polygon
@@ -48,17 +76,83 @@ contract CelerImplementationTest is BaseSetup {
         assertEq(celerImplementation.superChainId(137), 137);
     }
 
+    function test_revert_setChainId_invalidChainId_invalidCaller() public {
+        vm.expectRevert(Error.INVALID_CHAIN_ID.selector);
+        celerImplementation.setChainId(10, 0); /// optimism
+
+        vm.expectRevert(Error.INVALID_CHAIN_ID.selector);
+        celerImplementation.setChainId(0, 10); /// optimism
+
+        vm.expectRevert(Error.NOT_PROTOCOL_ADMIN.selector);
+        vm.prank(bond);
+        celerImplementation.setChainId(137, 137); /// polygon
+    }
+
     function test_broadcastPayload() public {
         /// @dev need to call setCelerBus(), setReceiver(), setChainId() before calling broadcastPayload(), 
         /// but we don't need to, as it's already done in BaseSetup
+        AMBMessage memory ambMessage;
+        BroadCastAMBExtraData memory ambExtraData;
+        address coreStateRegistry;
 
+        (ambMessage, ambExtraData, coreStateRegistry) = setupBroadcastPayloadAMBData(users[0]);
+
+        /// @dev only checking topic1 as it is the only one indexed in the Message event
+        vm.expectEmit(true, false, false, true, CELER_BUS);
+
+        /// @dev chainIds = [1, 56, 43114, 137, 42161, 10]; 
+        for (uint i = 1; i < chainIds.length; i++) {
+            emit Message(
+                address(celerImplementation),
+                celerImplementation.authorizedImpl(chainIds[i]),
+                chainIds[i],
+                abi.encode(ambMessage),
+                0.1 ether
+            );
+        }
+
+        vm.prank(coreStateRegistry);
+        celerImplementation.broadcastPayload{value: 0.1 ether}(users[0], abi.encode(ambMessage), abi.encode(ambExtraData));
+    }
+
+    function test_revert_broadcastPayload_invalidCaller() public {
+        AMBMessage memory ambMessage;
+        BroadCastAMBExtraData memory ambExtraData;
+        address coreStateRegistry;
+
+        (ambMessage, ambExtraData, coreStateRegistry) = setupBroadcastPayloadAMBData(users[0]);
+
+        vm.expectRevert(Error.INVALID_CALLER.selector);
+
+        vm.prank(bond);
+        celerImplementation.broadcastPayload{value: 0.1 ether}(users[0], abi.encode(ambMessage), abi.encode(ambExtraData));
+    }
+
+    function test_revert_broadcastPayload_gasRefundFailed() public {
+        AMBMessage memory ambMessage;
+        BroadCastAMBExtraData memory ambExtraData;
+        address coreStateRegistry;
+
+        /// @dev a contract that doesn't accept ETH
+        address dai = getContract(1, "DAI");
+
+        (ambMessage, ambExtraData, coreStateRegistry) = setupBroadcastPayloadAMBData(dai);
+
+        vm.expectRevert(Error.GAS_REFUND_FAILED.selector);
+
+        vm.prank(coreStateRegistry);
+        /// @dev note first arg to be dai
+        celerImplementation.broadcastPayload{value: 0.1 ether}(dai, abi.encode(ambMessage), abi.encode(ambExtraData));
+    }
+
+    function setupBroadcastPayloadAMBData(address _srcSender) public returns (AMBMessage memory, BroadCastAMBExtraData memory, address) {
         AMBMessage memory ambMessage = AMBMessage(
             DataLib.packTxInfo(
                 uint8(TransactionType.DEPOSIT), /// @dev TransactionType
                 uint8(CallbackType.INIT),
                 0, /// @dev isMultiVaults
                 1, /// @dev STATE_REGISTRY_TYPE,
-                users[0], /// @dev srcSender,
+                _srcSender, /// @dev srcSender,
                 chainId /// @dev srcChainId
             ),
             "" /// ambData
@@ -89,22 +183,6 @@ contract CelerImplementationTest is BaseSetup {
         /// @dev need to stop unused deployer prank, to use new prank, AND changePrank() doesn't work smh
         vm.stopPrank();
 
-        /// @dev only checking topic1 as it is the only one indexed in the Message event
-        vm.expectEmit(true, false, false, true, CELER_BUS);
-
-        /// @dev chainIds = [1, 56, 43114, 137, 42161, 10]; 
-        for (uint i = 1; i < chainIds.length; i++) {
-            emit Message(
-                address(celerImplementation),
-                celerImplementation.authorizedImpl(chainIds[i]),
-                chainIds[i],
-                abi.encode(ambMessage),
-                0.1 ether
-            );
-        }
-
-        vm.prank(coreStateRegistry);
-        celerImplementation.broadcastPayload{value: 0.1 ether}(users[0], abi.encode(ambMessage), abi.encode(ambExtraData));
+        return (ambMessage, ambExtraData, coreStateRegistry);
     }
-
 }
