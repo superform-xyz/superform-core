fde782c01d70ac3182963dc9e2d79f904201f10b
sujithsomraaj <somraajsujith@gmail.com>
2023-08-24 17:38:15 +0530

    feat: add scenarios using wormhole amb

---
+++ test/scenarios-deposit-singleDirectSingleVaultDeposit/4626.NoSwap.TokenInput.Slippage14.L1.t.sol
@@ -0,0 +1,65 @@
+/// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.19;
+
+// Test Utils
+import "../utils/ProtocolActions.sol";
+
+contract SDSVD4626NoSwapNativeSlippage14L1 is ProtocolActions {
+    function setUp() public override {
+        super.setUp();
+        /*//////////////////////////////////////////////////////////////
+                !! WARNING !!  DEFINE TEST SETTINGS HERE
+    //////////////////////////////////////////////////////////////*/
+        AMBs = [1, 4];
+
+        CHAIN_0 = AVAX;
+        DST_CHAINS = [AVAX];
+
+        /// @dev define vaults amounts and slippage for every destination chain and for every action
+        TARGET_UNDERLYINGS[AVAX][0] = [2];
+
+        TARGET_VAULTS[AVAX][0] = [0];
+
+        /// @dev id 0 is normal 4626
+
+        TARGET_FORM_KINDS[AVAX][0] = [0];
+
+        AMOUNTS[AVAX][0] = [2];
+
+        MAX_SLIPPAGE = 1000;
+
+        /// @dev 0 for no bridge, 1 for socket, 2 for lifi
+        LIQ_BRIDGES[AVAX][0] = [0];
+
+        actions.push(
+            TestAction({
+                action: Actions.Deposit,
+                multiVaults: false, //!!WARNING turn on or off multi vaults
+                user: 0,
+                testType: TestType.Pass,
+                revertError: "",
+                revertRole: "",
+                slippage: 999, // 0% <- if we are testing a pass this must be below each maxSlippage,
+                multiTx: false,
+                externalToken: 2 // 0 = DAI, 1 = USDT, 2 = WETH
+             })
+        );
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        SCENARIO TESTS
+    //////////////////////////////////////////////////////////////*/
+
+    function test_scenario() public {
+        for (uint256 act = 0; act < actions.length; act++) {
+            TestAction memory action = actions[act];
+            MultiVaultSFData[] memory multiSuperformsData;
+            SingleVaultSFData[] memory singleSuperformsData;
+            MessagingAssertVars[] memory aV;
+            StagesLocalVars memory vars;
+            bool success;
+
+            _runMainStages(action, act, multiSuperformsData, singleSuperformsData, aV, vars, success);
+        }
+    }
+}
