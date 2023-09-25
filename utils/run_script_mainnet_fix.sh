5502a69d6e3096bad707702fd907eed27cbe277f
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2023-09-25 10:23:35 +0100

    fix: lifiValidator

---
+++ utils/run_script_mainnet_fix.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Read the RPC URL
+source .env
+
+
+# Run the script
+echo Running...
+
+FOUNDRY_PROFILE=default forge script script/Deploy.LiFiValidator.s.sol:DeployContract --sig "run(uint256)" 0 --rpc-url $BSC_RPC_URL --broadcast --slow --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+FOUNDRY_PROFILE=default forge script script/Deploy.LiFiValidator.s.sol:DeployContract --sig "run(uint256)" 1 --rpc-url $POLYGON_RPC_URL --broadcast --slow --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+FOUNDRY_PROFILE=default forge script script/Deploy.LiFiValidator.s.sol:DeployContract --sig "run(uint256)" 2 --rpc-url $AVALANCHE_RPC_URL --broadcast --slow --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
