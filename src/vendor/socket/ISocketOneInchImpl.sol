829df3e9766215769f86b2d9965b8fe216562349
sujithsomraaj <somraajsujith@gmail.com>
2023-10-18 12:02:48 -0400

    chore: add socket one inch mock

--- src/vendor/socket/ISocketOneInchImpl.sol
+++ src/vendor/socket/ISocketOneInchImpl.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
+/// @title ISocketOneInchImpl
+/// @notice Interface for Socket's One Inch Impl
+/// @notice used for swaps without bridge
+/// @notice taken from https://polygonscan.com/address/0x2ddf16BA6d0180e5357d5e170eF1917a01b41fc0
 interface ISocketOneInchImpl {
     struct SwapInput {
         address fromToken;
@@ -9,4 +13,14 @@ interface ISocketOneInchImpl {
         uint256 amount;
         bytes swapExtraData;
     }
+
+    function performDirectAction(
+        address fromToken,
+        address toToken,
+        address receiver,
+        uint256 amount,
+        bytes memory swapExtraData
+    )
+        external
+        payable;
 }
---
+++ test/mocks/SocketOneInchMock.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.21;
+
+import "forge-std/Test.sol";
+
+/// local imports
+import "./MockERC20.sol";
+import { ISocketOneInchImpl } from "src/vendor/socket/ISocketOneInchImpl.sol";
+
+/// @title Socket OneInch Mock
+/// @dev eventually replace this by using a fork of the real registry contract
+contract SockeOneInchMock is ISocketOneInchImpl, Test {
+    address constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    receive() external payable { }
+
+    function performDirectAction(
+        address fromToken,
+        address toToken,
+        address receiver,
+        uint256 amount,
+        bytes memory swapExtraData
+    )
+        external
+        payable
+    {
+        (address from) = abi.decode(swapExtraData, (address));
+
+        if (fromToken != NATIVE) {
+            MockERC20(fromToken).transferFrom(from, address(this), amount);
+        } else {
+            require(msg.value == amount);
+        }
+
+        /// FIXME: account for slippage
+        deal(toToken, receiver, MockERC20(toToken).balanceOf(receiver) + amount);
+    }
+}
