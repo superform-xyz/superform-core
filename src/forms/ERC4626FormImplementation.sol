97638359f17a9777ce8c636fad85ec63894b52df
vikramarun <33469661+vikramarun@users.noreply.github.com>
2023-10-20 21:00:54 -0400

    chore: we come in pairs

--- src/forms/ERC4626FormImplementation.sol
+++ src/forms/ERC4626FormImplementation.sol
@@ -267,7 +267,7 @@ abstract contract ERC4626FormImplementation is BaseForm, LiquidityHandler {
         address asset = v.asset();
 
         /// @dev pulling from sender, to auto-send tokens back in case of failed deposits / reverts
-        IERC20(asset)).safeTransferFrom(msg.sender, address(this), singleVaultData_.amount);
+        IERC20(asset).safeTransferFrom(msg.sender, address(this), singleVaultData_.amount);
 
         /// @dev allowance is modified inside of the IERC20.transferFrom() call
         IERC20(asset).safeIncreaseAllowance(vaultLoc, singleVaultData_.amount);
