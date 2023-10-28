ac6bcfa46bd5bec12b4296a0798fc032a9aa10a0
vikramarun <33469661+vikramarun@users.noreply.github.com>
2023-10-27 20:49:39 -0400

    chore: fix emergency kyc

--- src/forms/ERC4626KYCDaoForm.sol
+++ src/forms/ERC4626KYCDaoForm.sol
@@ -93,9 +93,11 @@ contract ERC4626KYCDaoForm is ERC4626FormImplementation {
     }
 
     /// @inheritdoc BaseForm
-    function _emergencyWithdraw(address srcSender_, address refundAddress_, uint256 amount_) internal override {
-        _kycCheck(srcSender_);
-
+    function _emergencyWithdraw(address srcSender_, address refundAddress_, uint256 amount_)
+        internal
+        override
+        onlyKYC(srcSender_) 
+    {
         _processEmergencyWithdraw(refundAddress_, amount_);
     }
 
