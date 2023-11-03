b6725299d182ada5b41abc348a1b37a2f7702e37
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2023-11-03 18:29:47 +0000

    fix: post gerard deployment (#345)

---
+++ script/UpdatePaymentHelper.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.21;
+
+import { AbstractUpdatePaymentHelper } from "./Abstract.UpdatePaymentHelper.s.sol";
+
+contract UpdatePaymentHelper is AbstractUpdatePaymentHelper {
+    /*//////////////////////////////////////////////////////////////
+                        SELECT CHAIN IDS TO DEPLOY HERE
+    //////////////////////////////////////////////////////////////*/
+
+    uint64[] TARGET_DEPLOYMENT_CHAINS = [BSC, POLY, AVAX];
+
+    function updatePaymentHelper(uint256 selectedChainIndex) external {
+        uint256 trueIndex;
+        for (uint256 i = 0; i < chainIds.length; i++) {
+            if (TARGET_DEPLOYMENT_CHAINS[selectedChainIndex] == chainIds[i]) {
+                trueIndex = i;
+                break;
+            }
+        }
+
+        _updatePaymentHelper(
+            selectedChainIndex, trueIndex, Cycle.Prod, TARGET_DEPLOYMENT_CHAINS, TARGET_DEPLOYMENT_CHAINS
+        );
+    }
+}
