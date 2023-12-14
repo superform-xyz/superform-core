f6a843ec5c26b193d78f296816f4884e656003eb
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2023-12-14 18:06:49 +0000

    fix: comment

--- src/crosschain-liquidity/LiquidityHandler.sol
+++ src/crosschain-liquidity/LiquidityHandler.sol
@@ -53,6 +53,7 @@ abstract contract LiquidityHandler {
             token.safeIncreaseAllowance(bridge_, amount_);
         } else {
             if (nativeAmount_ < amount_) revert Error.INSUFFICIENT_NATIVE_AMOUNT();
+            if (nativeAmount_ > address(this).balance) revert Error.INSUFFICIENT_BALANCE();
         }
 
         (bool success,) = payable(bridge_).call{ value: nativeAmount_ }(txData_);
