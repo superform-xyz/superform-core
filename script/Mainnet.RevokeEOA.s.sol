0e68faee043897ae13ed1b0a07da453f92a22682
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2024-02-08 15:57:17 +0000

    fix: nit

--- script/Mainnet.RevokeEOA.s.sol
+++ script/Mainnet.RevokeEOA.s.sol
@@ -5,14 +5,13 @@ import { AbstractRevokeEOA } from "./Abstract.RevokeEOA.s.sol";
 
 contract RevokeEOAs is AbstractRevokeEOA {
     /*//////////////////////////////////////////////////////////////
-                        SELECT CHAIN IDS TO DEPLOY HERE
+                        SELECT CHAIN IDS TO REVOKE HERE
     //////////////////////////////////////////////////////////////*/
     uint64[] TARGET_DEPLOYMENT_CHAINS = [ETH, BSC, AVAX, POLY, ARBI, OP, BASE];
 
     ///@dev ORIGINAL SALT
     bytes32 constant salt = "SunNeverSetsOnSuperformRealm";
 
-    /// @dev stage 3 must be called only after stage 1 is complete for all chains!
     function revokeEOA(uint256 selectedChainIndex) external {
         uint256 trueIndex;
         for (uint256 i = 0; i < chainIds.length; i++) {
