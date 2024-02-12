81c62bc858c2e9bae93ef01d4ead3be805294a49
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2024-02-12 17:49:03 +0000

    chore: prep for staging deployment

--- script/Mainnet.Staging.Deploy.s.sol
+++ script/Mainnet.Staging.Deploy.s.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.23;
 
 import { AbstractDeploySingle } from "./Abstract.Deploy.Single.s.sol";
 
-contract MainnetDeploy is AbstractDeploySingle {
+contract MainnetStagingDeploy is AbstractDeploySingle {
     /*//////////////////////////////////////////////////////////////
                         SELECT CHAIN IDS TO DEPLOY HERE
     //////////////////////////////////////////////////////////////*/
-    uint64[] TARGET_DEPLOYMENT_CHAINS = [ETH, BSC, AVAX, POLY, ARBI, OP, BASE];
+    uint64[] TARGET_DEPLOYMENT_CHAINS = [BSC, ARBI, OP, BASE];
 
     ///@dev ORIGINAL SALT
-    bytes32 constant salt = "SunNeverSetsOnSuperformRealm";
+    bytes32 constant salt = "StagingV1_0";
 
     /// @notice The main stage 1 script entrypoint
     function deployStage1(uint256 selectedChainIndex) external {
@@ -23,8 +23,7 @@ contract MainnetDeploy is AbstractDeploySingle {
         SUPERFORM_RECEIVER = 0x1a6805487322565202848f239C1B5bC32303C2FE;
 
         _preDeploymentSetup();
-
-
+        
         uint256 trueIndex;
         for (uint256 i = 0; i < chainIds.length; i++) {
             if (TARGET_DEPLOYMENT_CHAINS[selectedChainIndex] == chainIds[i]) {
@@ -50,20 +49,5 @@ contract MainnetDeploy is AbstractDeploySingle {
         }
 
         _deployStage2(selectedChainIndex, trueIndex, Cycle.Prod, TARGET_DEPLOYMENT_CHAINS, TARGET_DEPLOYMENT_CHAINS);
-    }
-
-    /// @dev stage 3 must be called only after stage 1 is complete for all chains!
-    function deployStage3(uint256 selectedChainIndex) external {
-        _preDeploymentSetup();
-
-        uint256 trueIndex;
-        for (uint256 i = 0; i < chainIds.length; i++) {
-            if (TARGET_DEPLOYMENT_CHAINS[selectedChainIndex] == chainIds[i]) {
-                trueIndex = i;
-                break;
-            }
-        }
-
-        _deployStage3(selectedChainIndex, trueIndex, Cycle.Prod, TARGET_DEPLOYMENT_CHAINS);
     }
 }
---
+++ script/utils/run_script_mainnet_staging.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Note: How to set defaultKey - https://www.youtube.com/watch?v=VQe7cIpaE54
+
+# Read the RPC URL
+source .env
+
+# Run the script
+echo Running Stage 1: ...
+
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage1(uint256)" 0 --rpc-url $BSC_RPC_URL --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage1(uint256)" 1 --rpc-url $ARBITRUM_RPC_URL --slow --legacy --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage1(uint256)" 2 --rpc-url $OPTIMISM_RPC_URL --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage1(uint256)" 3 --rpc-url $BASE_RPC_URL --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+echo Running Stage 2: ...
+
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage2(uint256)" 0 --rpc-url $BSC_RPC_URL --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage2(uint256)" 1 --rpc-url $ARBITRUM_RPC_URL --slow --legacy --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage2(uint256)" 2 --rpc-url $OPTIMISM_RPC_URL --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
+
+wait
+FOUNDRY_PROFILE=default forge script script/Mainnet.Staging.Deploy.s.sol:MainnetStagingDeploy --sig "deployStage2(uint256)" 3 --rpc-url $BASE_RPC_URL --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
