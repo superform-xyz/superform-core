aa94589fef0c21594d89fe29b949bd42c4435d64
sujithsomraaj <somraajsujith@gmail.com>
2024-04-19 18:59:48 +0530

    chore: remove hashflow from tests

--- test/utils/BaseSetup.sol
+++ test/utils/BaseSetup.sol
@@ -55,7 +55,6 @@ import { DstSwapper } from "src/crosschain-liquidity/DstSwapper.sol";
 import { LiFiValidator } from "src/crosschain-liquidity/lifi/LiFiValidator.sol";
 import { SocketValidator } from "src/crosschain-liquidity/socket/SocketValidator.sol";
 import { DeBridgeValidator } from "src/crosschain-liquidity/deBridge/DeBridgeValidator.sol";
-import { HashflowValidator } from "src/crosschain-liquidity/hashflow/HashflowValidator.sol";
 
 import { SocketOneInchValidator } from "src/crosschain-liquidity/socket/SocketOneInchValidator.sol";
 import { LayerzeroImplementation } from "src/crosschain-data/adapters/layerzero/LayerzeroImplementation.sol";
@@ -108,7 +107,7 @@ abstract contract BaseSetup is DSTest, StdInvariant, Test {
     bytes32 public salt;
     mapping(uint64 chainId => mapping(bytes32 implementation => address at)) public contracts;
 
-    string[33] public contractNames = [
+    string[32] public contractNames = [
         "CoreStateRegistry",
         "TimelockStateRegistry",
         "BroadcastRegistry",
@@ -140,8 +139,7 @@ abstract contract BaseSetup is DSTest, StdInvariant, Test {
         "CanonicalPermit2",
         "EmergencyQueue",
         "SocketOneInchValidator",
-        "DeBridgeValidator",
-        "HashflowValidator"
+        "DeBridgeValidator"
     ];
 
     /*//////////////////////////////////////////////////////////////
@@ -591,10 +589,6 @@ abstract contract BaseSetup is DSTest, StdInvariant, Test {
             /// @dev 7.2.4- deploy deBridge validator
             vars.debridgeValidator = address(new DeBridgeValidator{ salt: salt }(vars.superRegistry));
             contracts[vars.chainId][bytes32(bytes("DeBridgeValidator"))] = vars.debridgeValidator;
-
-            /// @dev 7.2.5- deploy hashflow validator
-            vars.hashflowValidator = address(new HashflowValidator{ salt: salt }(vars.superRegistry));
-            contracts[vars.chainId][bytes32(bytes("HashflowValidator"))] = vars.hashflowValidator;
 
             /// @dev 7.3- kycDAO NFT used to test kycDAO vaults
             vars.kycDAOMock = address(new KYCDaoNFTMock{ salt: salt }());
