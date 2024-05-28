5939b57fd50b090b8d4c41e897f9fd4395f508f2
sujithsomraaj <somraajsujith@gmail.com>
2024-05-28 17:19:41 +0530

    chore: add smoke test for lz_v2 staging

--- test/mainnet/SmokeTest.Staging.t.sol
+++ test/mainnet/SmokeTest.Staging.t.sol
@@ -295,39 +295,28 @@ contract SmokeTestStaging is MainnetBaseSetup {
     }
 
     function test_layerzeroImplementation() public {
-        LayerzeroImplementation layerzero;
+        LayerzeroV2Implementation layerzero;
 
         /// @dev index should match the index of target chains
-        address[] memory endpoints = new address[](TARGET_DEPLOYMENT_CHAINS.length);
-        endpoints[0] = 0x3c2269811836af69497E5F486A85D7316753cf62;
-        endpoints[1] = 0x3c2269811836af69497E5F486A85D7316753cf62;
-        endpoints[2] = 0x3c2269811836af69497E5F486A85D7316753cf62;
-        endpoints[3] = 0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7;
-        endpoints[4] = 0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7;
-
-        /// @dev index should match the index of target chains
-        uint16[] memory ambIds_ = new uint16[](TARGET_DEPLOYMENT_CHAINS.length);
-        ambIds_[0] = uint16(102);
-        ambIds_[1] = uint16(110);
-        ambIds_[2] = uint16(111);
-        ambIds_[3] = uint16(184);
-        ambIds_[4] = uint16(112);
+        uint32[] memory ambIds_ = new uint32[](TARGET_DEPLOYMENT_CHAINS.length);
+        ambIds_[0] = uint16(30_102);
+        ambIds_[1] = uint16(30_110);
+        ambIds_[2] = uint16(30_111);
+        ambIds_[3] = uint16(30_184);
+        ambIds_[4] = uint16(30_112);
 
         for (uint256 i; i < TARGET_DEPLOYMENT_CHAINS.length; ++i) {
             uint64 chainId = TARGET_DEPLOYMENT_CHAINS[i];
             vm.selectFork(FORKS[chainId]);
-            layerzero = LayerzeroImplementation(getContract(chainId, "LayerzeroImplementation"));
+            layerzero = LayerzeroV2Implementation(getContract(chainId, "LayerzeroImplementation"));
 
-            assertEq(address(layerzero.lzEndpoint()), endpoints[i]);
+            assertEq(address(layerzero.endpoint()), lzV2Endpoint);
 
             for (uint256 j; j < TARGET_DEPLOYMENT_CHAINS.length; ++j) {
                 if (chainId != TARGET_DEPLOYMENT_CHAINS[j]) {
                     assertEq(
-                        layerzero.trustedRemoteLookup(ambIds_[j]),
-                        abi.encodePacked(
-                            getContract(TARGET_DEPLOYMENT_CHAINS[j], "LayerzeroImplementation"),
-                            getContract(TARGET_DEPLOYMENT_CHAINS[i], "LayerzeroImplementation")
-                        )
+                        layerzero.peers(ambIds_[j]),
+                        bytes32(uint256(uint160(getContract(TARGET_DEPLOYMENT_CHAINS[j], "LayerzeroImplementation"))))
                     );
                     assertEq(layerzero.ambChainId(TARGET_DEPLOYMENT_CHAINS[j]), ambIds_[j]);
                     assertEq(layerzero.superChainId(ambIds_[j]), TARGET_DEPLOYMENT_CHAINS[j]);
