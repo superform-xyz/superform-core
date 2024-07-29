1bb7abfe5525957c2f11386d332de41c6a494e9e
sujithsomraaj <somraajsujith@gmail.com>
2024-07-29 15:53:27 +0530

    chore: optimize 7540 form; 1096 -> 940 lines

--- src/forms/interfaces/IERC7540Form.sol
+++ src/forms/interfaces/IERC7540Form.sol
@@ -2,12 +2,67 @@
 pragma solidity ^0.8.23;
 
 import { IERC4626Form } from "./IERC4626Form.sol";
-import { InitSingleVaultData, TimelockPayload, LiqRequest } from "../../types/DataTypes.sol";
+import { LiqRequest } from "../../types/DataTypes.sol";
 import { SyncWithdrawTxDataPayload } from "../../interfaces/IAsyncStateRegistry.sol";
 
 /// @title IERC7540FormBase
 /// @author Zeropoint Labs
 interface IERC7540FormBase {
+    //////////////////////////////////////////////////////////////
+    //                          EVENTS                           //
+    //////////////////////////////////////////////////////////////
+
+    /// @dev is emitted when a payload is processed by the destination contract.
+    event RequestProcessed(
+        uint64 indexed srcChainID,
+        uint64 indexed dstChainId,
+        uint256 indexed srcPayloadId,
+        uint256 amount,
+        address vault,
+        uint256 requestId
+    );
+
+    //////////////////////////////////////////////////////////////
+    //                         ERRORS                         //
+    //////////////////////////////////////////////////////////////
+    /// @dev Vault must be set by calling forward dust to paymaster once
+    error VAULT_KIND_NOT_SET();
+
+    /// @dev Only async state registry can perform this operation
+    error NOT_ASYNC_STATE_REGISTRY();
+
+    /// @dev Error thrown if the check to erc165 async deposit interface failed
+    error ERC_165_INTERFACE_DEPOSIT_CALL_FAILED();
+
+    /// @dev Error thrown if the check to erc165 async redeem interface failed
+    error ERC_165_INTERFACE_REDEEM_CALL_FAILED();
+
+    /// @dev Error thrown if the vault does not support async deposit nor redeem
+    error VAULT_NOT_SUPPORTED();
+
+    /// @dev Error thrown if the vault kind is invalid for the operation
+    error INVALID_VAULT_KIND();
+
+    /// @dev Error thrown if trying to forward share token
+    error CANNOT_FORWARD_SHARES();
+
+    /// @dev If functions not implemented within the ERC7540 Standard
+    error NOT_IMPLEMENTED();
+
+    /// @dev If redeemed assets fell off slippage in redeem 2nd step
+    error REDEEM_INVALID_LIQ_REQUEST();
+
+    //////////////////////////////////////////////////////////////
+    //                        ENUM                              //
+    //////////////////////////////////////////////////////////////
+
+    enum VaultKind {
+        UNSET,
+        DEPOSIT_ASYNC,
+        REDEEM_ASYNC,
+        FULLY_ASYNC
+    }
+
     //////////////////////////////////////////////////////////////
     //              EXTERNAL VIEW FUNCTIONS                    //
     //////////////////////////////////////////////////////////////
