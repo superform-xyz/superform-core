fd4600210655ff92de424a08eca9a219c9a3a58c
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2024-08-02 08:46:09 +0100

    fix: rollback changes

--- src/forms/wrappers/ERC5115To4626WrapperFactory.sol
+++ src/forms/wrappers/ERC5115To4626WrapperFactory.sol
@@ -97,8 +97,8 @@ contract ERC5115To4626WrapperFactory is IERC5115To4626WrapperFactory {
 
     /// @inheritdoc IERC5115To4626WrapperFactory
     function batchCreateWrappersWithSuperform(
-        uint32 formImplementationId_,
-        address underlyingVaultAddress_,
+        uint32[] calldata formImplementationIds_,
+        address[] calldata underlyingVaultAddresses,
         address[] calldata tokenIns,
         address[] calldata tokenOuts
     )
@@ -106,16 +106,20 @@ contract ERC5115To4626WrapperFactory is IERC5115To4626WrapperFactory {
         override
         returns (address[] memory wrappers_)
     {
-        uint256 len = tokenIns.length;
+        uint256 len = underlyingVaultAddresses.length;
 
-        if (len != tokenOuts.length) {
+        if (
+            len != tokenIns.length || tokenIns.length != tokenOuts.length
+                || tokenOuts.length != formImplementationIds_.length
+        ) {
             revert Error.ARRAY_LENGTH_MISMATCH();
         }
 
         wrappers_ = new address[](len);
         for (uint256 i; i < len; i++) {
-            wrappers_[i] =
-                _createWrapperWithSuperform(formImplementationId_, underlyingVaultAddress_, tokenIns[i], tokenOuts[i]);
+            wrappers_[i] = _createWrapperWithSuperform(
+                formImplementationIds_[i], underlyingVaultAddresses[i], tokenIns[i], tokenOuts[i]
+            );
         }
     }
 
