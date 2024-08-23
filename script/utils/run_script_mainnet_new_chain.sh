9d7a90da32f5fd68a02f06186459311d79fe42bc
sujithsomraaj <somraajsujith@gmail.com>
2024-08-23 15:08:48 +0900

    chore: fix typo in new chain script

--- script/utils/run_script_mainnet_new_chain.sh
+++ script/utils/run_script_mainnet_new_chain.sh
@@ -218,4 +218,3 @@ echo Revoke burner address: ...
 FOUNDRY_PROFILE=production forge script script/forge-scripts/misc/Mainnet.Deploy.NewChain.s.sol:MainnetDeployNewChain --sig "revokeBurnerAddress(uint256,uint256)" 0 0 --rpc-url $FANTOM_RPC_URL --broadcast --slow --account defaultKey --sender 0x48aB8AdF869Ba9902Ad483FB1Ca2eFDAb6eabe92
 
 wait
-x
