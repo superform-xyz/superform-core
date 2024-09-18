461d74f651be8e80a157fcab807f16f27ad3d92c
sujithsomraaj <somraajsujith@gmail.com>
2024-09-18 19:25:48 +0530

    chore: create one multisig tx to configure both send and receive lib from blast

--- script/forge-scripts/EnvironmentUtils.s.sol
+++ script/forge-scripts/EnvironmentUtils.s.sol
@@ -37,6 +37,7 @@ abstract contract EnvironmentUtils is AbstractDeploySingle {
             TARGET_CHAINS.push(BASE);
             TARGET_CHAINS.push(FANTOM);
             TARGET_CHAINS.push(LINEA);
+            TARGET_CHAINS.push(BLAST);
 
             if (useNewSalt) {
                 salt = "SunNeverSetsOnSuperformRealmV2";
