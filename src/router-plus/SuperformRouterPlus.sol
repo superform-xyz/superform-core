1ab28aa9ed7c03febf95d3addd7969351b860c27
Tamara Ringas <69479754+TamaraRingas@users.noreply.github.com>
2024-10-24 17:32:15 +0200

    chore: comments

--- src/router-plus/SuperformRouterPlus.sol
+++ src/router-plus/SuperformRouterPlus.sol
@@ -521,6 +521,7 @@ contract SuperformRouterPlus is ISuperformRouterPlus, BaseSuperformRouterPlus {
         uint256 assetsBalanceAfter = asset.balanceOf(address(this));
         balanceDifference = assetsBalanceAfter - assetsBalanceBefore;
 
+        /// @dev validate the tolerance
         if (assets < TOLERANCE_CONSTANT || balanceDifference < TOLERANCE_CONSTANT) revert TOLERANCE_EXCEEDED();
 
         /// @dev validate the slippage
