e367a5d4b3a1d0195ad865a04abf994522db4bfd
Timepunk <45543880+0xTimepunk@users.noreply.github.com>
2024-12-09 09:11:07 +0000

    feat: router plus on mainnet eth (#663)

--- script/forge-scripts/misc/Abstract.Deploy.RouterPlus.s.sol
+++ script/forge-scripts/misc/Abstract.Deploy.RouterPlus.s.sol
@@ -65,4 +65,104 @@ abstract contract AbstractDeployRouterPlus is EnvironmentUtils {
             );
         }
     }
+
+    function _deployRouterPlus(
+        uint256 env,
+        uint256 i,
+        uint256 trueIndex,
+        Cycle cycle,
+        uint64[] memory finalDeployedChains
+    )
+        internal
+        setEnvDeploy(cycle)
+    {
+        assert(salt.length > 0);
+        UpdateVars memory vars;
+
+        vars.chainId = finalDeployedChains[i];
+
+        cycle == Cycle.Dev ? vm.startBroadcast(deployerPrivateKey) : vm.startBroadcast();
+
+        address superRegistry = _readContractsV1(env, chainNames[trueIndex], vars.chainId, "SuperRegistry");
+        address expectedSr;
+
+        if (env == 0) {
+            expectedSr = vars.chainId == 250
+                ? 0x7feB31d18E43E2faeC718EEd2D7f34402c3e27b4
+                : 0x17A332dC7B40aE701485023b219E9D6f493a2514;
+        } else {
+            expectedSr = vars.chainId == 250
+                ? 0x7B8d68f90dAaC67C577936d3Ce451801864EF189
+                : 0xB2C097ac459aFAc892ae5b35f6bd6a9Dd3071F47;
+        }
+
+        assert(superRegistry == expectedSr);
+
+        address superformRouterPlus = address(new SuperformRouterPlus{ salt: salt }(superRegistry));
+        contracts[vars.chainId][bytes32(bytes("SuperformRouterPlus"))] = superformRouterPlus;
+
+        console.log("superformRouterPlus", superformRouterPlus);
+        // address superformRouterPlusAsync = address(new SuperformRouterPlusAsync{ salt: salt }(superRegistry));
+        // contracts[vars.chainId][bytes32(bytes("SuperformRouterPlusAsync"))] = superformRouterPlusAsync;
+
+        vm.stopBroadcast();
+
+        /// @dev we use normal export contract to not override v1 contracts
+        for (uint256 j = 0; j < contractNames.length; j++) {
+            _exportContract(
+                chainNames[trueIndex], contractNames[j], getContract(vars.chainId, contractNames[j]), vars.chainId
+            );
+        }
+    }
+
+    function _configureRouterPlusProd(
+        uint256 env,
+        uint256 i,
+        uint256 trueIndex,
+        Cycle cycle,
+        uint64[] memory finalDeployedChains
+    )
+        internal
+        setEnvDeploy(cycle)
+    {
+        assert(salt.length > 0);
+        UpdateVars memory vars;
+
+        vars.chainId = finalDeployedChains[i];
+
+        address superRegistry = _readContractsV1(env, chainNames[trueIndex], vars.chainId, "SuperRegistry");
+        address expectedSr;
+
+        if (env == 0) {
+            expectedSr = vars.chainId == 250
+                ? 0x7feB31d18E43E2faeC718EEd2D7f34402c3e27b4
+                : 0x17A332dC7B40aE701485023b219E9D6f493a2514;
+        } else {
+            expectedSr = vars.chainId == 250
+                ? 0x7B8d68f90dAaC67C577936d3Ce451801864EF189
+                : 0xB2C097ac459aFAc892ae5b35f6bd6a9Dd3071F47;
+        }
+
+        assert(superRegistry == expectedSr);
+
+        address superformRouterPlus = _readContractsV1(env, chainNames[trueIndex], vars.chainId, "SuperformRouterPlus");
+        assert(superformRouterPlus != address(0));
+        bytes memory txn = abi.encodeWithSelector(
+            SuperRegistry.setAddress.selector, keccak256("SUPERFORM_ROUTER_PLUS"), superformRouterPlus, vars.chainId
+        );
+        addToBatch(superRegistry, 0, txn);
+
+        // address superformRouterPlusAsync =
+        //     _readContractsV1(env, chainNames[trueIndex], vars.chainId, "SuperformRouterPlusAsync");
+        // assert(superformRouterPlusAsync != address(0));
+        // txn = abi.encodeWithSelector(
+        //     SuperRegistry.setAddress.selector,
+        //     keccak256("SUPERFORM_ROUTER_PLUS_ASYNC"),
+        //     superformRouterPlusAsync,
+        //     vars.chainId
+        // );
+        // addToBatch(superRegistry, 0, txn);
+
+        executeBatch(vars.chainId, PROTOCOL_ADMINS[trueIndex], manualNonces[trueIndex], true);
+    }
 }
--- script/forge-scripts/misc/Mainnet.Deploy.SuperformRouterPlus.s.sol
+++ script/forge-scripts/misc/Mainnet.Deploy.SuperformRouterPlus.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import { AbstractDeployRouterPlus } from "./Abstract.Deploy.RouterPlus.s.sol";
 
 contract MainnetDeployRouterPlus is AbstractDeployRouterPlus {
-    function deployRouterPlus(uint256 env, uint256 selectedChainIndex, uint256 useNewSalt) external {
+    function deployRouterPlusStaging(uint256 env, uint256 selectedChainIndex, uint256 useNewSalt) external {
         _setEnvironment(env, useNewSalt == 1 ? true : false);
         _preDeploymentSetup();
 
@@ -17,6 +17,38 @@ contract MainnetDeployRouterPlus is AbstractDeployRouterPlus {
         }
         if (env == 1) {
             _deployRouterPlusStaging(env, selectedChainIndex, trueIndex, Cycle.Prod, TARGET_CHAINS);
+        }
+    }
+
+    function deployRouterPlusProd(uint256 env, uint256 selectedChainIndex, uint256 useNewSalt) external {
+        _setEnvironment(env, useNewSalt == 1 ? true : false);
+        _preDeploymentSetup();
+
+        uint256 trueIndex;
+        for (uint256 i = 0; i < chainIds.length; i++) {
+            if (TARGET_CHAINS[selectedChainIndex] == chainIds[i]) {
+                trueIndex = i;
+                break;
+            }
+        }
+        if (env == 0) {
+            _deployRouterPlus(env, selectedChainIndex, trueIndex, Cycle.Prod, TARGET_CHAINS);
+        }
+    }
+
+    function configureRouterPlusProd(uint256 env, uint256 selectedChainIndex, uint256 useNewSalt) external {
+        _setEnvironment(env, useNewSalt == 1 ? true : false);
+        _preDeploymentSetup();
+
+        uint256 trueIndex;
+        for (uint256 i = 0; i < chainIds.length; i++) {
+            if (TARGET_CHAINS[selectedChainIndex] == chainIds[i]) {
+                trueIndex = i;
+                break;
+            }
+        }
+        if (env == 0) {
+            _configureRouterPlusProd(env, selectedChainIndex, trueIndex, Cycle.Prod, TARGET_CHAINS);
         }
     }
 }
